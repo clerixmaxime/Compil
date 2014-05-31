aaf34a377f7279035639a0490145a41d48e2adfc
clerixmaxime <clerixmaxime@users.noreply.github.com>
2014-05-31 12:42:50 +0200

    Create syntaxique.h

---
+++ syntaxique.h
@@ -0,0 +1,8 @@
+#include "lexical.h"
+
+int prog(FILE* source);
+int inst(FILE* source);
+int suiteInst(FILE* source);
+int expr(FILE* source);
+int atome(FILE* source);
+int resteExpr(FILE* source);
