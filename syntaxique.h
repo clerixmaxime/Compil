25903734aeaafa7122bdeb6ad7bf7c4a09cca92c
clerixmaxime <clerixmaxime@users.noreply.github.com>
2014-06-01 12:16:13 +0200

    Update syntaxique.h
    Ajout paramètre par

--- syntaxique.h
+++ syntaxique.h
@@ -3,6 +3,6 @@
 int prog(FILE* source);
 int inst(FILE* source);
 int suiteInst(FILE* source);
-int expr(FILE* source);
+int expr(FILE* source, int par);
 int atome(FILE* source);
-int resteExpr(FILE* source);
+int resteExpr(FILE* source, int par);
